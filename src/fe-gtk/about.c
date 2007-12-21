2d2db14b0e59690b8220c6a89b6c7d4376824161
William Pitcock <nenolod@atheme.org>
2007-12-21 03:12:47 -0600

    about.c: rewrite entirely using GtkAboutDialog class.

--- src/fe-gtk/about.c
+++ src/fe-gtk/about.c
@@ -1,5 +1,5 @@
-/* X-Chat
- * Copyright (C) 1998 Peter Zelezny.
+/* Conspire
+ * Copyright (c) 2008 William Pitcock <nenolod@sacredspiral.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,16 +24,7 @@
 
 #include "fe-gtk.h"
 
-#include <gtk/gtkmain.h>
-#include <gtk/gtkcontainer.h>
-#include <gtk/gtkdialog.h>
-#include <gtk/gtkvbox.h>
-#include <gtk/gtkhbox.h>
-#include <gtk/gtkimage.h>
-#include <gtk/gtklabel.h>
-#include <gtk/gtkbutton.h>
-#include <gtk/gtkstock.h>
-#include <gtk/gtkwindow.h>
+#include <gtk/gtk.h>
 
 #include "../common/xchat.h"
 #include "../common/util.h"
@@ -42,77 +33,53 @@
 #include "gtkutil.h"
 #include "about.h"
 
+static const gchar *authors[] = {
+	"William Pitcock <nenolod@sacredspiral.co.uk>",
+	"Kiyoshi Aman <kiyoshi.aman@gmail.com>",
+	"Based on XChat by Peter Zelezny <zed@xchat.org>",
+	NULL
+};
 
-static GtkWidget *about = 0;
-
-static int
-about_close (void)
-{
-	about = 0;
-	return 0;
-}
+static const gchar license[] = 
+	"This program is free software; you can redistribute it and/or modify "
+	"it under the terms of the GNU General Public License as published by "
+	"the Free Software Foundation; either version 2 of the License, or "
+	"(at your option) any later version.\n"
+	"\n"
+	"This program is distributed in the hope that it will be useful, "
+	"but WITHOUT ANY WARRANTY; without even the implied warranty of "
+	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+	"GNU General Public License for more details.\n"
+	"\n"
+	"You should have received a copy of the GNU General Public License "
+	"along with this program; if not, write to the Free Software "
+	"Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.";
 
 void
 menu_about (GtkWidget * wid, gpointer sess)
 {
 	GdkPixbuf *about_pix;
-	GtkWidget *vbox, *label, *hbox;
-	char buf[512];
-	const char *locale = NULL;
 	extern GtkWindow *parent_window;      /* maingui.c */
 
-	if (about)
-	{
-		gtk_window_present (GTK_WINDOW (about));
-		return;
-	}
-
-	about = gtk_dialog_new ();
-	gtk_window_set_position (GTK_WINDOW (about), GTK_WIN_POS_CENTER);
-	gtk_window_set_resizable (GTK_WINDOW (about), FALSE);
-	gtk_window_set_title (GTK_WINDOW (about), _("About conspire"));
-	if (parent_window)
-		gtk_window_set_transient_for (GTK_WINDOW (about), parent_window);
-	g_signal_connect (G_OBJECT (about), "destroy",
-							G_CALLBACK (about_close), 0);
-
-	vbox = GTK_DIALOG (about)->vbox;
-
 	about_pix = gdk_pixbuf_scale_simple(pix_conspire, 128, 128, GDK_INTERP_BILINEAR);
-	wid = gtk_image_new_from_pixbuf (about_pix);
-	gtk_container_add (GTK_CONTAINER (vbox), wid);
+
+	gtk_show_about_dialog(parent_window,
+#if GTK_CHECK_VERSION (2, 11, 0)
+		"program-name", "Conspire",
+#else
+		"name", "Conspire",
+#endif
+		"authors", authors,
+		"license", license,
+		"wrap-license", TRUE,
+		"logo", about_pix,
+		"copyright", "Copyright (c) 2007-2008 William Pitcock\n"
+			     "Copyright (c) 2007-2008 Kiyoshi Aman\n"
+			     "Copyright (c) 1998-2007 Peter Zelezny",
+		"version", PACKAGE_VERSION,
+		"website", "http://www.nenolod.net/conspire",
+		"comments", _("An advanced graphical IRC client."),
+		NULL);
+
 	g_object_unref(about_pix);
-
-	label = gtk_label_new (NULL);
-	gtk_label_set_selectable (GTK_LABEL (label), TRUE);
-	gtk_container_add (GTK_CONTAINER (vbox), label);
-	g_get_charset (&locale);
-	(snprintf) (buf, sizeof (buf),
-				"<span size=\"x-large\"><b>Conspire "PACKAGE_VERSION"</b></span>\n\n"
-				"%s\n\n"
-				"%s\n"
-				"<b>Charset</b>: %s "
-				"<b>GTK+</b>: %i.%i.%i\n"
-				"<b>Compiled</b>: "__DATE__"\n\n"
-				"<small>Based on XChat by \302\251 1998-2007 Peter \305\275elezn\303\275 &lt;zed@xchat.org></small>",
-					_("IRC for the Atheme Desktop"),
-					get_cpu_str(),
-					locale,
-					gtk_major_version,
-					gtk_minor_version,
-					gtk_micro_version);
-	gtk_label_set_markup (GTK_LABEL (label), buf);
-	gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_CENTER);
-
-	hbox = gtk_hbox_new (0, 2);
-	gtk_container_add (GTK_CONTAINER (vbox), hbox);
-
-	wid = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-	GTK_WIDGET_SET_FLAGS (GTK_WIDGET (wid), GTK_CAN_DEFAULT);
-	gtk_box_pack_end (GTK_BOX (GTK_DIALOG (about)->action_area), wid, 0, 0, 0);
-	gtk_widget_grab_default (wid);
-	g_signal_connect (G_OBJECT (wid), "clicked",
-							G_CALLBACK (gtkutil_destroy), about);
-
-	gtk_widget_show_all (about);
 }
