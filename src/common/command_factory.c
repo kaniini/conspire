94881a518d519bb9eb30a19c8f9495d90b270362
William Pitcock <nenolod@atheme.org>
2008-03-15 22:06:18 -0500

    New command API. Now all we need to do is hook it up. :D

---
+++ src/common/command_factory.c
@@ -0,0 +1,102 @@
+/* Conspire
+ * Copyright (C) 2008 William Pitcock
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#include "command_factory.h"
+
+mowgli_dictionary_t *cmd_dict_ = NULL;
+
+void
+command_register(const gchar *name, const gchar *description, const gchar *helptext, CommandFlags flags, CommandHandler handler)
+{
+	Command *cmd;
+
+	if (cmd_dict_ == NULL)
+		cmd_dict_ = mowgli_dictionary_create(g_ascii_strcasecmp);
+
+	if (!(cmd = mowgli_dictionary_retrieve(cmd_dict_, name)))
+	{
+		cmd = g_slice_new0(Command);
+		cmd->description = g_strdup(description);
+		cmd->helptext = g_strdup(helptext);
+		cmd->flags = flags;
+	}
+
+	g_assert(cmd != NULL);
+
+	cmd->handlers = g_list_append(cmd->handlers, handler);
+}
+
+void
+command_remove_handler(const gchar *name, CommandHandler handler)
+{
+	Command *cmd;
+
+	if (!(cmd = mowgli_dictionary_retrieve(cmd_dict_, name)))
+		return;
+
+	cmd->handlers = g_list_remove(cmd->handlers, handler);
+}
+
+void
+command_set_flags(const gchar *name, CommandFlags flags)
+{
+	Command *cmd;
+
+	if (!(cmd = mowgli_dictionary_retrieve(cmd_dict_, name)))
+		return;
+
+	cmd->flags = flags;
+}
+
+CommandFlags
+command_get_flags(const gchar *name)
+{
+	Command *cmd;
+
+	if (!(cmd = mowgli_dictionary_retrieve(cmd_dict_, name)))
+		return CMD_NO_FLAGS;
+
+	return cmd->flags;
+}
+
+CommandExecResult
+command_execute(struct session *sess, const gchar *name, char *tbuf, char *word[], char *word_eol[])
+{
+	Command *cmd;
+	GList *node;
+
+	if (!(cmd = mowgli_dictionary_retrieve(cmd_dict_, name)))
+		return COMMAND_EXEC_NOCMD;
+
+	MOWGLI_ITER_FOREACH(node, cmd->handlers)
+	{
+		CommandResult ret;
+		CommandHandler handler;
+
+		handler = (CommandHandler) node->data;
+		ret = handler(sess, tbuf, word, word_eol);
+
+		if (ret == CMD_EXEC_FAIL)
+			return COMMAND_EXEC_FAILED;
+
+		if (ret == CMD_EXEC_STOP)
+			continue;
+	}
+
+	return COMMAND_EXEC_OK;
+}
--- src/common/command_factory.h
+++ src/common/command_factory.h
@@ -16,14 +16,17 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
+#include "xchat.h"
+
 #ifndef __CONSPIRE_COMMAND_FACTORY_H__GUARD
 #define __CONSPIRE_COMMAND_FACTORY_H__GUARD
 
 typedef enum {
-    CMD_HANDLE_QUOTES = (1 << 0),
-    CMD_NEED_CHANNEL = (1 << 1),
-    CMD_NEED_SERVER = (1 << 2),
-    CMD_STOP_ON_FAIL = (1 << 3),
+    CMD_NO_FLAGS = 0,
+    CMD_HANDLE_QUOTES = (1 << 1),
+    CMD_NEED_CHANNEL = (1 << 2),
+    CMD_NEED_SERVER = (1 << 3),
+    CMD_STOP_ON_FAIL = (1 << 4),
 } CommandFlags;
 
 typedef struct {
@@ -39,12 +42,18 @@ typedef enum {
     CMD_EXEC_STOP
 } CommandResult;
 
+typedef enum {
+    COMMAND_EXEC_OK,
+    COMMAND_EXEC_FAILED,
+    COMMAND_EXEC_NOCMD
+} CommandExecResult;
+
 typedef CommandResult (*CommandHandler)(struct session * sess, char *tbuf, char *word[], char *word_eol[]);
 
 void command_register(const gchar *name, const gchar *description, const gchar *helptext, CommandFlags flags, CommandHandler handler);
 void command_remove_handler(const gchar *name, CommandHandler handler);
 void command_set_flags(const gchar *name, CommandFlags flags);
 CommandFlags command_get_flags(const gchar *name);
-void command_execute(struct session *sess, const gchar *name, char *tbuf, char *word[], char *word_eol[]);
+CommandExecResult command_execute(struct session *sess, const gchar *name, char *tbuf, char *word[], char *word_eol[]);
 
 #endif
