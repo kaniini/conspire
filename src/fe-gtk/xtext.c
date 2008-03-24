ec2f6eeccb9e438ba9bf617d88041edb666552cc
William Pitcock <nenolod@atheme.org>
2008-03-24 16:08:24 -0500

    Enforce proper boundaries for non-HTML colors.

--- src/fe-gtk/xtext.c
+++ src/fe-gtk/xtext.c
@@ -2254,8 +2254,9 @@ gtk_xtext_render_str (GtkXText * xtext, int y, textentry * ent,
 			xtext->in_hilight = TRUE;
 		}
 
-		if ((xtext->parsing_color && ((xtext->parsing_htmlcolor && isxdigit (str[i])) || isdigit(str[i]) || str[i] == '#') && xtext->nc < 7) ||
-			 (xtext->parsing_color && str[i] == ',' && ((xtext->parsing_htmlcolor && isxdigit (str[i])) || isdigit(str[i+1]) || str[i+1] == '#') && !xtext->parsing_backcolor && xtext->nc < 8))
+		/* XXX this is ugly rewrite it --nenolod */
+		if ((xtext->parsing_color && ((xtext->parsing_htmlcolor && isxdigit (str[i])) || isdigit(str[i]) || str[i] == '#') && xtext->nc < xtext->parsing_htmlcolor ? 7 : 2) ||
+			 (xtext->parsing_color && str[i] == ',' && ((xtext->parsing_htmlcolor && isxdigit (str[i])) || isdigit(str[i+1]) || str[i+1] == '#') && !xtext->parsing_backcolor && xtext->nc < xtext->parsing_htmlcolor ? 8 : 3))
 		{
 			pstr++;
 			if (str[i] == ',')
