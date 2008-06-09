b7be1c96fea38a106d9c542fb9ec800f8bb632b1
William Pitcock <nenolod@atheme.org>
2008-06-08 20:57:55 -0500

    Add tcp_sendf_now().

--- src/common/server.c
+++ src/common/server.c
@@ -232,9 +232,7 @@ void
 tcp_sendf (server *serv, char *fmt, ...)
 {
 	va_list args;
-	/* keep this buffer in BSS. Converting UTF-8 to ISO-8859-x might make the
-      string shorter, so allow alot more than 512 for now. */
-	static char send_buf[1540];	/* good code hey (no it's not overflowable) */
+	static char send_buf[2048];
 	int len;
 
 	va_start (args, fmt);
@@ -246,6 +244,24 @@ tcp_sendf (server *serv, char *fmt, ...)
 		len = strlen (send_buf);
 
 	tcp_send_len (serv, send_buf, len);
+}
+
+void
+tcp_sendf_now(server *serv, char *fmt, ...)
+{
+	va_list args;
+	static char send_buf[2048];
+	int len;
+
+	va_start (args, fmt);
+	len = vsnprintf(send_buf, sizeof (send_buf) - 1, fmt, args);
+	va_end (args);
+
+	send_buf[sizeof (send_buf) - 1] = '\0';
+	if (len < 0 || len > (sizeof (send_buf) - 1))
+		len = strlen (send_buf);
+
+	server_send_real(serv, send_buf, len);
 }
 
 static int
