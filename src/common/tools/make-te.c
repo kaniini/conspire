fe35269d13def6d1d9d575ca95be06f003419a54
William Pitcock <nenolod@atheme.org>
2009-03-25 23:12:16 -0500

    make-te.c: Make this build what we want again.

--- src/common/tools/make-te.c
+++ src/common/tools/make-te.c
@@ -17,42 +17,20 @@ int main()
 	while(fgets(name, sizeof(name), stdin))
 	{
 		name[strlen(name)-1] = 0;
-		fgets(num, sizeof(num), stdin);
-		num[strlen(num)-1] = 0;
-		fgets(help, sizeof(help), stdin);
-		help[strlen(help)-1] = 0;
 		fgets(def, sizeof(def), stdin);
 		def[strlen(def)-1] = 0;
 		fgets(args, sizeof(args), stdin);
 		args[strlen(args)-1] = 0;
 		fgets(buf, sizeof(buf), stdin);
 
-		if (args[0] == 'n')
-			printf("\n{\"%s\", %s, %d, \n\"%s\"},\n",
-							 name, help, atoi(args+1) | 128, def);
-		else
-			printf("\n{\"%s\", %s, %d, \nN_(\"%s\")},\n",
-							 name, help, atoi(args), def);
+		printf("\n{\"%s\", %d, \n\"%s\"},\n",
+			 name, atoi(args), def);
+
 		defines[i] = strdup (num);
 		i++;
 	}
 
 	printf("};\n");
 	
-	fprintf(stderr, "/* this file is auto generated, edit textevents.in instead! */\n\nenum\n{\n");
-	max = i;
-	i = 0;
-	while (i < max)
-	{
-		if (i + 1 < max)
-		{
-			fprintf(stderr, "\t%s,\t\t%s,\n", defines[i], defines[i+1]);
-			i++;
-		} else
-			fprintf(stderr, "\t%s,\n", defines[i]);
-		i++;
-	}
-	fprintf(stderr, "\tNUM_XP\n};\n");
-
 	return 0;
 }
