84b89d2952e6ecbcead1e5f6b794c390a75b589e
Tony Vroon <chainsaw@gentoo.org>
2009-04-11 21:31:49 +0100

    Backed out changeset 531ae57dcb9f because it is *broken*.

--- src/common/cap.h
+++ src/common/cap.h
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
-#include "conspire-config.h"
 #include "xchat.h"
 
 #ifndef __CONSPIRE__LIBCONSPIRE__CAP_H__GUARD
