cd66367bfa6cc8cc6e6a5852420b3930f2e70717
Noah Fontes <nfontes@transtruct.org>
2009-04-11 16:24:57 -0400

    Include conspire-config.h in cap.h

--- src/common/cap.h
+++ src/common/cap.h
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
+#include "conspire-config.h"
 #include "xchat.h"
 
 #ifndef __CONSPIRE__LIBCONSPIRE__CAP_H__GUARD
